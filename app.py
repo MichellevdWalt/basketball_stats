d0db3a7da0a449ad43a97f7e137a5292b898e1f3
MichellevdWalt <michellevdwalt13@gmail.com>
2020-09-17 09:28:41 +0200

    Loop added

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import copy
 # if __name__ == "__main__":
 
 teams_c = copy.deepcopy(constants.TEAMS)
-
 players_c = copy.deepcopy(constants.PLAYERS)
 
 def clean_data():
@@ -17,15 +16,9 @@ def clean_data():
             player["experience"] = False
         guardian = player["guardians"].split("and")
         player["guardians"] = guardian
-
-
 clean_data()
 
-#balance experienced and inexperienced
-#split players into experienced and inexperienced. Then divide by len(teams_c) and divide exp and inexp equally
-#inexp players per team =  int(len(inexp_players)/len(teams_c))
-#exp players per tem = int(len(exp_players)/len(teams_c) 
-#for team in teams_c: teams[0][team] = edit start and end for both sets of data
+
 def balance_teams():
     exp_players = []
     inexp_players = []
@@ -50,61 +43,95 @@ def balance_teams():
 
 teams = balance_teams()
 
+while True:
+    print("\nBASKETBALL TEAM STATS TOOL")
+    print ("\n---Menu---\n")
+    print("Here are your choices: \n1.) Display team stats. \n2.) Quit")
+    option = input("Enter an option   ")
 
-print("BASKETBALL TEAM STATS TOOL")
-print ("\n---Menu---\n")
-print("Here are your choices: \n1.) Display team stats. \n2.) Quit")
-option = input("Enter an option   ")
+    if option == "1":
+        print("\nPlease choose from these teams:")
+        for index, team in enumerate(teams[0], 1):
+            print(f'{index}.) {team}')
+        print(f'{len(teams_c) + 1}.) Quit')
+    
 
-if option == "1":
-    print("\nPlease choose from these teams:")
-    for index, team in enumerate(teams[0], 1):
-        print(f'{index}.) {team}')
-    print(f'{len(teams_c) + 1}.) Quit')
-   
+    def exp(team):
+        exp_player = []
+        for player in teams[0][team]:
+            if player["experience"]:
+                exp_player.append(player)
+        return exp_player
 
-def exp(team):
-    exp_player = []
-    for player in teams[0][team]:
-        if player["experience"]:
-            exp_player.append(player)
-    return exp_player
+        
+    def inexp(team):
+        inexp_player = []
+        for player in teams[0][team]:
+            if player["experience"] == False:
+                inexp_player.append(player)
+        return inexp_player
+
+
+    def average_height(team):
+        total_height = 0
+        for player in teams[0][team]:
+            total_height += player["height"]
+        average_height = total_height/len(teams[0][team])
+        return average_height
+
+
+    def print_team(option):
+        team_chosen = teams_c[int(option)-1]
+        team_sentence = f'\nTeam: {team_chosen} Stats'
+        exp_players = exp(team_chosen)
+        inexp_players = inexp(team_chosen)
+        avg_height = average_height(team_chosen)
+        print(team_sentence)
+        print("-"*len(team_sentence))
+        print(f"Total Players: {len(teams[0][team_chosen])}")
+        print(f"Experienced Players: {len(exp_players)} ")
+        print(f"Inexperienced Players: {len(inexp_players)}")
+        print(f"Average Height of Players: {avg_height}inches")
+        print("\nPlayers on team:")
+        players = []
+        for player in teams[0][team_chosen]:
+            players.append(player["name"])
+        players_sentence = ', '.join(players)
+        print(f'  {players_sentence}')
+        guardians = []
+        for player in teams[0][team_chosen]:
+            guardians += (player["guardians"])
+        print(f"\nTeam Guardians: \n  {', '.join(guardians)}")
+
+    while True:
+        option = input("Please enter an option   ")
+        if option == "1" or option == "2" or option == "3":
+            print_team(option)
+            break
+        elif option == "4":
+            print("\nThanks for using our stats tool! Goodbye")
+            break
+        else:
+            print("\nPlease enter a valid numerical option  ")
+            continue
 
     
-def inexp(team):
-    inexp_player = []
-    for player in teams[0][team]:
-        if player["experience"] == False:
-            inexp_player.append(player)
-    return inexp_player
 
-
-
-def print_team(option):
-    team_chosen = teams_c[int(option)-1]
-    team_sentence = f'\nTeam: {team_chosen} Stats'
-    exp_players = exp(team_chosen)
-    inexp_players = inexp(team_chosen)
-    print(team_sentence)
-    print("-"*len(team_sentence))
-    print(f"Total Players: {len(teams[0][team_chosen])}")
-    print(f"Experienced Players: {len(exp_players)} ")
-    print(f"Inexperienced Players: {len(inexp_players)}")
-    print("\nPlayers on team:")
-    players = []
-    for player in teams[0][team_chosen]:
-        players.append(player["name"])
-    players_sentence = ', '.join(players)
-    print(f'  {players_sentence}')
-
-while True:
-    option = input("Please enter an option   ")
-    if option == "1" or option == "2" or option == "3":
-        print_team(option)
-        break
-    elif option == "4":
-        print("\nThanks for using our stats tool! Goodbye")
-        break
-    else:
-        print("\nPlease enter a valid numerical option  ")
+    while True:
+        would_continue = input("\nWould you like to Continue? y or n  ")
+        if would_continue.upper() == "Y":
+            restart = True
+            break
+        elif would_continue.upper() == "N":
+            restart = False
+            break
+        else:
+            print("\nPlease enter a y for yes or a n for no. ")
+            continue
+    
+    if restart:
         continue
+    elif restart == False:
+        print("\nThanks for using our Basketball Stats Tool. Goodbye")
+        break
+
